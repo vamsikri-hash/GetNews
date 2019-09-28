03563d1264c7df9997e4c0ad9e3d7df164ce74bc
vamsikri-hash <vamsikrishna3415@outlook.in>
2019-09-28 13:19:49 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,1 @@
+#simple page to get news from api
---
+++ app.js
@@ -0,0 +1,61 @@
+const sports=document.querySelector("#sports");
+const tech=document.querySelector("#tech");
+const entert=document.querySelector("#entert");
+sports.addEventListener("click",getsports);
+tech.addEventListener("click",gettech);
+entert.addEventListener("click",getenter);
+const apiarry=["https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=de371855895d444a9eac47444d7a6abe",
+               "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=de371855895d444a9eac47444d7a6abe",
+               "https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=de371855895d444a9eac47444d7a6abe"
+              ]
+var id;
+function getsports(){
+id=0;
+fetchnews();
+}
+function gettech() {
+	id=1;
+	fetchnews();
+}
+function getenter()
+{
+	id=2;
+	fetchnews();
+}
+
+
+function fetchnews()
+{
+ 
+  	api=apiarry[id];
+
+  fetch(api).then(response=>response.json())
+            .then(function(respJSON) {
+                  if (respJSON.status == "ok") {
+                  	          console.log(respJSON);
+                              displayNews(respJSON);
+                    } else {
+                         console.log("not ok");
+                        }
+                  
+               })
+            .catch(e=>console.log(e));
+
+}
+
+ function displayNews(respJSON) {
+        const articles = respJSON.articles;
+        var articlesHTML = "";
+
+        for (const i in articles) {
+          const article = articles[i];
+          articlesHTML += `<div><ul>
+          <li> <img class="featured-image" src="${article.urlToImage}"/></li>
+          <li> <a class="title" href="${ article.url }">${article.title}</a></li>
+          <li class="author">Source: ${article.source.name}</li>
+          
+          </ul></div>`;
+        }
+
+        document.getElementById("news-container").innerHTML = articlesHTML;
+      }
